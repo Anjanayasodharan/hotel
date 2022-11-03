b8f93745cae295b9777e2f43ee69f065670c2ed5
anjanayasodharan89@gmail.com <anjanayasodharan89@gmail.com>
2022-11-03 09:41:28 +0530

    first commit

---
+++ bill.py
@@ -0,0 +1,18 @@
+while(True):
+    print("\n please select an option")
+    print("1. Tea")
+    print("2. Coffee")
+    print("3. Snaks")
+    print("4. Burger")
+    print("5. Exit")
+    ch=int(input("enter the choice"))
+    if(ch==1):
+        print("Added tea")
+    elif(ch==2):
+        print("Added Coffee")
+    elif(ch==3):
+        print("Added Snaks")
+    elif(ch==4):
+        print("Added Burger")
+    elif(ch==5):
+        break
